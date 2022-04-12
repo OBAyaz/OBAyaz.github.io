22882c4d14b0b29f3672e05205b45ba89cd02acc
Baris <baris.ayaz@mimosa.co>
2022-04-12 23:15:49 +0300

    test

--- chunk-1.js
+++ chunk-1.js
@@ -1,1 +1,21 @@
 console.log("Hello from chunk-1")
+
+const resultDOM = document.getElementById('response')
+
+const getData = () => {
+  resultDOM.innerText = "Loading..."
+  fetch('https://jsonplaceholder.typicode.com/todos/')
+    .then( response => {
+      console.log(response.json())
+      resultDOM.innerText = response.json()
+    })
+    .then(data => {
+      resultDOM.innerText = "Success"
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+    .finally(() => {
+      resultDOM.innerText = "Crashed"
+    })
+}
--- chunk-2.js
+++ chunk-2.js
@@ -1,1 +1,28 @@
 console.log("Hello from chunk-2")
+
+const resultDOM2 = document.getElementById('response2')
+
+const getData2 = () => {
+  resultDOM2.innerText = "Loading..."
+  fetch('https://jsonplaceholder.typicode.com/todos/')
+    .then( response => {
+      let data = response.json()
+      resultDOM2.innerText = data
+      return data
+    })
+    .then(data => {
+      resultDOM2.innerText = filterData(data)
+    })
+    .catch((e) => {
+      console.log(e)
+      resultDOM2.innerText = "Crashed"
+    })
+    .finally(() => {
+    })
+}
+
+const filterData = (data) => {
+
+  return data[0].user["userId"]
+
+}
